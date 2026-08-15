a4c0dc067737f2f5fa3437de42f44522b7713e1e
JoseMIaau <jose.tontito.si@gmail.com>
2026-08-15 17:40:08 -0400

    Implementado metodo para cargar archivo en la clase GestorArchivo

--- src/persistencia/GestorArchivo.java
+++ src/persistencia/GestorArchivo.java
@@ -1,6 +1,8 @@
 package persistencia;
 import modelo.Producto;
+import modelo.Categorias;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
@@ -43,6 +45,26 @@ public class GestorArchivo {
         if (!archivo.exists()) {
             return productos; 
         }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))){
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                if (linea.trim().isEmpty()) continue;
+                String[] p = linea.split(separador);
+                if (p.length == 5) {
+                    int id = Integer.parseInt(p[0].trim());
+                    String nombre = p[1].trim();
+                    double precio = Double.parseDouble(p[2].trim());
+                    int stock = Integer.parseInt(p[3].trim());
+                    Categorias categoria = Categorias.valueOf(p[4].trim().toUpperCase());
+
+                    productos.add(new Producto(id, nombre, precio, stock, categoria));
+                }
+            }
+        }   catch (IOException | NumberFormatException e) {
+            System.err.println("Error al cargar el archivo: " + e.getMessage());
+        }
+
         return productos;
     }
 
