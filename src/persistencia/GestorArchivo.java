e8b43719e1ac8da4f9892d8bd00ebf6936809e95
JoseMIaau <jose.tontito.si@gmail.com>
2026-08-15 11:01:03 -0400

    Modificacion clase GestorArchivo y creación de clase Inventario
    Se modificó la clase de gestor de archivo implementando sus atributos, constructor y el metodo de guardaCatalogo, se creo el metodo cargarCatalogo pero solo imolementa la logica de cuando no existe un archivo de momento

--- src/persistencia/GestorArchivo.java
+++ src/persistencia/GestorArchivo.java
@@ -1,5 +1,49 @@
 package persistencia;
+import modelo.Producto;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorArchivo {
-    
+    private String rutaArchivo;
+    private static final String separador = ",";
+
+    public GestorArchivo(String rutaArchivo){
+        this.rutaArchivo = rutaArchivo;
+    }
+
+    public void guardarCatalogo(List<Producto> productos){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(rutaArchivo))){
+            for (Producto p : productos){
+                String linea = p.getId()+separador+
+                                p.getNombre()+separador+
+                                p.getPrecio()+separador+
+                                p.getStock()+separador+
+                                p.getCategoria();
+                bw.write(linea);
+                bw.newLine();            
+            }
+
+
+        } catch (IOException e){
+            System.err.println("Error al guardar en el archivo: " + e.getMessage());
+        }
+    }
+
+    public List<Producto> cargarCatalogo(){
+        List<Producto> productos = new ArrayList<>();
+        File archivo = new File(rutaArchivo);
+
+        if (!archivo.exists()) {
+            return productos; 
+        }
+        return productos;
+    }
+
 }
