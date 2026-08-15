1b868082d4f2fc470dbefa776a9dd7fa794518d4
JoseMIaau <jose.tontito.si@gmail.com>
2026-08-15 16:59:51 -0400

    Singleton en inventario

--- src/modelo/Inventario.java
+++ src/modelo/Inventario.java
@@ -1,5 +1,24 @@
 package modelo;
+import persistencia.GestorArchivo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Inventario {
-    
+    private static Inventario instancia;
+    private List<Producto> productos;
+    private GestorArchivo gestorArchivo;
+
+    private Inventario() {
+        this.gestorArchivo = new GestorArchivo("inventario.csv");
+        this.productos = gestorArchivo.cargarCatalogo();
+    }
+
+    public static Inventario getInstancia() {
+        if (instancia == null) {
+            instancia = new Inventario();
+        }
+        return instancia;
+    }
+
+    //Metodos CRUD
 }
