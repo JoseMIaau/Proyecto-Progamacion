50257549bd9bf64fee8ee21ca803075525fb87f3
javiwisiddi <javiignaciaortega@gmail.com>
2026-08-18 16:52:01 -0400

    falso commit

--- src/persistencia/GestorArchivo.java
+++ src/persistencia/GestorArchivo.java
@@ -3,9 +3,7 @@ import modelo.Producto;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
