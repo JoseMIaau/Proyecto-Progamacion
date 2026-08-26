b07539a2be9c3c8c45dfb5da13ffd42306b62934
JoseMIaau <jose.tontito.si@gmail.com>
2026-08-25 21:42:25 -0400

    Iniciar el programa desde el main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import vista.BaseFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+            }
+            new BaseFrame().setVisible(true);
+        });
     }
 }
